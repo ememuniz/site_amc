03bf4b2b08dd3cba095de34a5bf21b2912a77b39
Emerson Muniz <emersonmuniz2013@gmail.com>
2026-05-01 05:02:32 -0300

    atualização de gestao de conteudo do membro

--- frontend/src/components/elements/GestaoConteudo.jsx
+++ frontend/src/components/elements/GestaoConteudo.jsx
@@ -7,6 +7,16 @@ export default function GestaoConteudo() {
   return (
     <div className='gestao-conteudo'>
       <DashboardHeader />
+      <section className='gestao-conteudo-content'>
+        <div className='gestao-conteudo-content-title'>
+          <h1>MEU VLOG</h1>
+        </div>
+        <hr className='linha-divisoria'/>
+        <div className='gestao-conteudo-content-addvlog'>
+          <h2>GESTÃO DE POSTAGENS DE VLOG</h2>
+          <a href="#"><span>+ </span>Novo Registro</a>
+        </div>
+      </section>
     </div>
   );
 }
